cecf18ae7537a5adc8c40aa8db42a4e1fd201df3
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-04-16 09:52:16 +0700

    Initial Commit

---
+++ OpenSpaceTests/OpenSpaceTests.swift
@@ -0,0 +1,18 @@
+//
+//  OpenSpaceTests.swift
+//  OpenSpaceTests
+//
+//  Created by Bambang Tri Rahmat Doni on 16/04/26.
+//
+
+import Testing
+
+struct OpenSpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
