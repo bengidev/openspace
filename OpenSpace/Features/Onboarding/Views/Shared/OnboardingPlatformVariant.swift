40814d7d1f9032d877685b667df7443df2a28d00
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-04-23 13:18:15 +0700

    Simplify onboarding shared identifiers

--- OpenSpace/Features/Onboarding/Views/Shared/OnboardingPlatformVariant.swift
+++ OpenSpace/Features/Onboarding/Views/Shared/OnboardingPlatformVariant.swift
@@ -5,8 +5,6 @@
 //  Created by Codex on 18/04/26.
 //
 
-import SwiftUI
-
 enum OnboardingPlatformVariant {
   case ios
   case ipad
@@ -20,24 +18,6 @@ enum OnboardingPlatformVariant {
       "onboarding.ipad"
     case .mac:
       "onboarding.mac"
-    }
-  }
-
-  var panelStyle: OnboardingHeroPanelStyle {
-    switch self {
-    case .mac:
-      .desktopCanvas
-    case .ios, .ipad:
-      .floatingShowcase
-    }
-  }
-
-  var usesFloatingPanelEffect: Bool {
-    switch self {
-    case .mac:
-      false
-    case .ios, .ipad:
-      true
     }
   }
 }
