fee640db6dd34f6eac9b245da4f86a28b9c7d16a
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-04-24 11:26:59 +0700

    Update tests for feature reducers

--- OpenSpaceTests/OpenSpaceTests.swift
+++ OpenSpaceTests/OpenSpaceTests.swift
@@ -5,14 +5,36 @@
 //  Created by Bambang Tri Rahmat Doni on 16/04/26.
 //
 
+import ComposableArchitecture
 import Testing
+@testable import OpenSpace
 
 struct OpenSpaceTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        // Swift Testing Documentation
-        // https://developer.apple.com/documentation/testing
+  @Test
+  func promptChangedClearsHighlightedQuickPromptWhenTextDiverges() async {
+    let store = TestStore(initialState: WorkspaceFeature.State(
+      selectedPrompt: WorkspaceQuickPrompt.toDoList.rawValue,
+      highlightedQuickPrompt: .toDoList
+    )) {
+      WorkspaceFeature()
     }
 
+    await store.send(.promptChanged("Draft a release summary")) {
+      $0.selectedPrompt = "Draft a release summary"
+      $0.highlightedQuickPrompt = nil
+    }
+  }
+
+  @Test
+  func quickPromptTappedUpdatesPromptAndFocus() async {
+    let store = TestStore(initialState: WorkspaceFeature.State()) {
+      WorkspaceFeature()
+    }
+
+    await store.send(.quickPromptTapped(.articleSummary)) {
+      $0.highlightedQuickPrompt = .articleSummary
+      $0.selectedPrompt = WorkspaceQuickPrompt.articleSummary.rawValue
+      $0.isPromptFocused = true
+    }
+  }
 }
