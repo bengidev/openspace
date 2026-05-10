e72e5575c71cacddcd07451ccbb5780a0c236527
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-05-10 18:07:22 +0700

    feat(SpacerPet): add Expression domain enum

---
+++ OpenSpace/Features/SpacerPet/Domain/SpacerPetExpression.swift
@@ -0,0 +1,14 @@
+//
+//  SpacerPetExpression.swift
+//  OpenSpace
+//
+
+enum SpacerPetExpression: Equatable {
+    case calm
+    case curious
+    case focused
+    case happy
+    case excited
+    case wink
+    case starry
+}
