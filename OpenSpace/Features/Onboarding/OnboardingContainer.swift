0d692db11892ba0a75a7cd18bd791dac0c129987
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-05-12 09:17:57 +0700

    feat(onboarding): add OnboardingContainer
    Introduce OnboardingContainer to wire up onboarding flow state
    with the new presenter layer.

---
+++ OpenSpace/Features/Onboarding/OnboardingContainer.swift
@@ -0,0 +1,69 @@
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct OnboardingContainer {
+    @ObservableState
+    struct State: Equatable {
+        var isFinished = false
+        var flow = OnboardingFlowState()
+    }
+
+    @CasePathable
+    enum Action: Equatable {
+        case onAppear
+        case finishTapped
+        case skipTapped
+        case themeToggleTapped
+        case flow(OnboardingFlowAction)
+    }
+
+    var body: some Reducer<State, Action> {
+        Scope(state: \.flow, action: \.flow) {
+            OnboardingFlow()
+        }
+
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .send(.flow(.onAppear))
+            case .finishTapped:
+                return .send(.flow(.finishTapped))
+            case .skipTapped:
+                return .send(.flow(.skipTapped))
+            case .themeToggleTapped:
+                return .none
+            case let .flow(.onboardingAlreadyCompleted(completed)):
+                state.isFinished = completed
+                return .none
+            case .flow(.finishTapped):
+                state.isFinished = true
+                return .none
+            case .flow(.themeToggleTapped):
+                return .send(.themeToggleTapped)
+            case .flow:
+                return .none
+            }
+        }
+    }
+}
+
+struct OnboardingContainerView: View {
+    @Bindable var store: StoreOf<OnboardingContainer>
+
+    var body: some View {
+        OnboardingView(
+            store: store.scope(state: \.flow, action: \.flow),
+            onThemeToggle: { store.send(.themeToggleTapped) }
+        )
+    }
+}
+
+#Preview {
+    OnboardingContainerView(
+        store: Store(initialState: OnboardingContainer.State()) {
+            OnboardingContainer()
+        }
+    )
+    .environment(\.palette, OpenSpacePalette.resolve(.dark))
+}
