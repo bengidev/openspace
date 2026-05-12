d6605dba97b781004bfbd9141023cbd2ce0a06ce
Bambang Tri Rahmat Doni <bambang.trd17@gmail.com>
2026-05-12 11:00:02 +0700

    refactor(spacerpet): hoist SpacerPetContainerView above SpacerPetContainer reducer

--- OpenSpace/Features/SpacerPet/SpacerPetContainer.swift
+++ OpenSpace/Features/SpacerPet/SpacerPetContainer.swift
@@ -1,18 +1,27 @@
 import ComposableArchitecture
 import SwiftUI
 
+struct SpacerPetContainerView: View {
+    @Bindable var store: StoreOf<SpacerPetContainer>
+    
+    var body: some View {
+        SpacerPetOverlay(store: store.scope(state: \.feature, action: \.feature))
+    }
+}
+
+
 @Reducer
 struct SpacerPetContainer {
     @ObservableState
     struct State: Equatable {
         var feature = SpacerPetFeatureState()
     }
-
+    
     @CasePathable
     enum Action: Equatable {
         case feature(SpacerPetFeatureAction)
     }
-
+    
     var body: some Reducer<State, Action> {
         Scope(state: \.feature, action: \.feature) {
             SpacerPetFeature()
@@ -20,10 +29,3 @@ struct SpacerPetContainer {
     }
 }
 
-struct SpacerPetContainerView: View {
-    @Bindable var store: StoreOf<SpacerPetContainer>
-
-    var body: some View {
-        SpacerPetOverlay(store: store.scope(state: \.feature, action: \.feature))
-    }
-}
